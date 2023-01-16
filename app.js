b46739df79251ff78ac35de4d522fc681a14973c
Sargon <sergegabriel24@gmail.com>
2023-01-16 11:36:56 +0100

    added an object'

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var Animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
+"use strict";
+const Animals = ["apa", "orangutang", "gorilla", "get", "giraff"];
 console.log(Animals);
+const employee = {
+    name: "tony",
+    age: 23,
+    anställd: true
+};
+console.log(employee);
--- app.ts
+++ app.ts
@@ -3,3 +3,17 @@
 const Animals: string [] = ["apa", "orangutang", "gorilla", "get", "giraff"];
 
 console.log(Animals);
+
+type obj = {
+    name: string;
+    age: number;
+    anställd: boolean;
+}
+
+const employee : obj = {
+    name : "tony",
+    age: 23,
+    anställd: true
+}
+
+console.log(employee);
